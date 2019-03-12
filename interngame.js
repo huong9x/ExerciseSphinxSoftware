93f4e4cac586cbcf43c4a959d77e6117747d2790
huong9x <huongnv@sphinx-software.com>
2019-03-12 08:37:29 +0700

    sphinx homework 11/03 fizzbuzz game

--- interngame.js
+++ interngame.js
@@ -1,6 +1,6 @@
-for (var i=1; i < 101; i++){
-  if (i % 15 == 0) console.log("Sphinx Software");
-  else if (i % 3 == 0) console.log("Sphinx");
-  else if (i % 5 == 0) console.log("Software");
-  else console.log(i);
+for (var i=1; i <= 100; i++){
+ if (i % 3 == 0&&i % 5 == 0) document.write("Sphinx Software<br>");
+ else if (i % 3 == 0) document.write("Sphinx<br>");
+ else if (i % 5 == 0) document.write("Software<br>");
+ else document.write(i+"<br>");
 }
