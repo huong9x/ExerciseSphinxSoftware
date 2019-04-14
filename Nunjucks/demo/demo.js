6517c08f350cd5d8007fedace4b6c645224920e9
huong9x <huongnv@sphinx-software.com>
2019-04-14 21:57:42 +0700

    a temp solution for search user in database

--- Nunjucks/demo/demo.js
+++ Nunjucks/demo/demo.js
@@ -3,12 +3,14 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
 var router = new Router();
+const configuration = require('./knexfile')['development'];    // require environment's settings from knexfile
+const knex = require('knex')(configuration); 
 
-
-router.get('/hello/:name',(context,next) => {
-context.body = nunjucks.render('index.html',{name: context.params.name});
+router.get('/search/:name',async (context,next) => {
+    let items = await knex.select('*').from('users').where('user', 'like', '%'+context.params.name+'%');
+    context.body = nunjucks.render('index.html',{items});
 });
 
 
 app.use(router.routes());
-app.listen(8080);
+app.listen(8081);
